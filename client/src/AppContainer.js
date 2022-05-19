a58b527ac41b80f35ed59f0936b0d3f6fb0659cd
Sanidhya Desai <sdesai@costar.com>
2022-05-19 14:31:02 -0400

    adding chat container

--- client/src/AppContainer.js
+++ client/src/AppContainer.js
@@ -1,8 +1,11 @@
 import * as React from "react";
 import "./app.css";
+import ChatContainer from "./components/chat-container";
 
 export const AppContainer = () => {
-  return <span className="bg-green">hello world</span>;
+  return (
+  <><ChatContainer /><span className="bg-green">hello world</span></>
+  );
 };
 
 export default AppContainer;
